025c76ca7c84eaecbf8b81896fc05d20af86ed56
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2023-11-25 23:43:02 +0300

    Container With Most Water. Done

--- container_with_the_most_water/ContainerWithTheMostWater.java
+++ container_with_the_most_water/ContainerWithTheMostWater.java
@@ -1,7 +1,32 @@
 package container_with_the_most_water;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ContainerWithTheMostWater {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+
+    public static int maxArea(int[] height) {
+        int maxWaterVolume = 0;
+        int cursor = height.length - 1;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = cursor; j > i; j--) {
+                if (height[i] >= height[j]) {
+                    maxWaterVolume = Math.max(height[j]*(j - i), maxWaterVolume);
+                    continue;
+                } if (height[i] < height[j]) {
+                    cursor = j;
+                    maxWaterVolume = Math.max(height[i]*(j - i), maxWaterVolume);
+                    break;
+                }
+            }
+        }
+        return maxWaterVolume;
     }
 }

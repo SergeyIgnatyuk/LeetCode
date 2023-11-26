060ffc33e192abcf105d96533a058523386fba65
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2023-11-26 14:49:53 +0300

    Squares of a Sorted Array

---
+++ squares_of_a_sorted_array/SortedArraySquares.java
@@ -0,0 +1,17 @@
+package squares_of_a_sorted_array;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+ */
+public class SortedArraySquares {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-4,-1,0,3,10})));;
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums).map(i -> i*i).sorted().toArray();
+    }
+}

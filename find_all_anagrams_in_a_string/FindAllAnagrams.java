dbc4b083a13ea6f90d57e3506419473bbb2dd4de
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2023-11-26 23:11:59 +0300

    Find All Anagrams in a String. Used sorting

--- find_all_anagrams_in_a_string/FindAllAnagrams.java
+++ find_all_anagrams_in_a_string/FindAllAnagrams.java
@@ -4,37 +4,30 @@ import java.util.*;
 
 /**
  * Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
- *
+ * <p>
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
  */
 
 public class FindAllAnagrams {
     public static void main(String[] args) {
-        System.out.println(findAnagrams("cbaebaaaacbabacd", "abc"));
+        System.out.println(findAnagrams("abab", "ab"));
     }
 
-    public static List<String> findAnagrams(String s, String p) {
-        List<String> list = new ArrayList<>();
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
         char[] pArray = p.toCharArray();
-        char[] sArray = s.toCharArray();
-        StringTokenizer tokenizer = new StringTokenizer(s, p);
-        int startIndex = 0;
-        while (tokenizer.hasMoreTokens()) {
-            String token = tokenizer.nextToken();
-            String anagram = s.substring(startIndex, s.indexOf(token));
-            if (anagram.length() == p.length()) {
-                list.add(anagram);
-            } else {
-                int count = 0;
-                for (int i = 0; i < pArray.length; i++) {
-                    for (int j = 0; j < sArray.length; j++) {
-                        if (pArray[i] == sArray[j]) {
-                            count++;
-                        }
-                    }
+        Arrays.sort(pArray);
+        String sortedP = Arrays.toString(pArray);
+        for (int i = 0; i < s.length(); i++) {
+            if (i + p.length() <= s.length()) {
+                String possibleAnagram = s.substring(i, i + p.length());
+                char[] anagramArray = possibleAnagram.toCharArray();
+                Arrays.sort(anagramArray);
+                String sortedAnagram = Arrays.toString(anagramArray);
+                if (sortedAnagram.equals(sortedP)) {
+                    list.add(i);
                 }
             }
-            startIndex = s.indexOf(token) + 1;
         }
         return list;
     }

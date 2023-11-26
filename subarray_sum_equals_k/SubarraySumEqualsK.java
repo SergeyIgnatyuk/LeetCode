a0f0bc4db79ee2ca7974fbf2c335a58a13fef0c7
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2023-11-26 18:14:23 +0300

    Subarray Sum Equals K

---
+++ subarray_sum_equals_k/SubarraySumEqualsK.java
@@ -0,0 +1,32 @@
+package subarray_sum_equals_k;
+
+/**
+ * Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+ * <p>
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ */
+
+public class SubarraySumEqualsK {
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{1, -1, 0}, 0));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+
+                if (j == nums.length - 1) {
+                    sum = 0;
+                }
+            }
+        }
+        return count;
+    }
+}

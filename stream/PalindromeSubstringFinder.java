17c2da82ea05dce38f82a134af0439e4275d2b36
Sergey Ignatyuk <signatuk89@gmail.com>
2024-02-16 15:22:12 +0300

    PalindromeSubstringFinder in progress

--- stream/PalindromeSubstringFinder.java
+++ stream/PalindromeSubstringFinder.java
@@ -1,10 +1,23 @@
 package stream;
 
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Дана строка. Палиндром - это строка, которая читается одинаково как с начала, так и с конца.
  * Необходимо написать программу на Java с использованием Stream API для поиска всех подстрок в строке, которые являются палиндромами.
  */
 public class PalindromeSubstringFinder {
-    String input = "abcbadefdedxyzzyxdetrewert";
 
+    public static void main(String[] args) {
+        String input = "abcbadefdedxyzzyxdetrewert";
+        Map<Integer, String> substrings = IntStream
+                .range(0, input.length())
+                .mapToObj(i ->  new AbstractMap.SimpleImmutableEntry<>(i, String.valueOf(input.toCharArray()[i])))
+                .collect(Collectors.toMap(AbstractMap.SimpleImmutableEntry::getKey, AbstractMap.SimpleImmutableEntry::getValue));
+    }
 }

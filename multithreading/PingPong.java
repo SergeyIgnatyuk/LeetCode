05e925c95baaf30a606107c0f262d882a1626fb1
Sergey Ignatyuk <signatuk89@gmail.com>
2024-02-21 14:55:19 +0300

    Ping Pong

---
+++ multithreading/PingPong.java
@@ -0,0 +1,52 @@
+package multithreading;
+
+public class PingPong {
+    private static volatile String prevPunch = "pong";
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread ping = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    synchronized (lock) {
+                        while (prevPunch.equals("ping")) {
+                            lock.wait();
+                        }
+                        System.out.println("ping");
+                        prevPunch = "ping";
+                        lock.notify();
+                    }
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        Thread pong = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    synchronized (lock) {
+                        while (prevPunch.equals("pong")) {
+                            lock.wait();
+                        }
+                        System.out.println("pong");
+                        prevPunch = "pong";
+                        lock.notify();
+                    }
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        ping.setDaemon(true);
+        pong.setDaemon(true);
+        ping.start();
+        pong.start();
+
+        Thread.sleep(5000);
+    }
+}

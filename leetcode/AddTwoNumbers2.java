2a203d650b59628766893da8d67097dc22b78531
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2025-08-05 13:10:07 +0300

    AddTwoNumbers2

---
+++ leetcode/AddTwoNumbers2.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+ * order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+public class AddTwoNumbers2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
